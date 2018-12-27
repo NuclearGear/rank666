e1a1e921365a5a5b1a39b668d6a0a8ea90d97482
levislin <levislin2016@gmail.com>
2018-12-27 14:51:36 +0800

    1

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'du',
+    'database'        => 'www_91rank_com',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'www_91rank_com',
     // 密码
-    'password'        => 'root',
+    'password'        => 'rKAcAiSdna',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
