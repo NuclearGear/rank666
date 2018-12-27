e172d8f9ed02ee129f9c2f992cfae3c51d5184d7
levislin <levislin2016@gmail.com>
2018-12-27 14:44:52 +0800

    数据库初始化

--- application/database.php
+++ application/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'du',
+    'database'        => 'www_91rank_com',
     // 用户名
     'username'        => 'www_91rank_com',
     // 密码
