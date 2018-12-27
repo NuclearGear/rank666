8f673c5b221301881b1830b49b5d83d2da4540a8
levislin <levislin2016@gmail.com>
2018-12-27 14:41:38 +0800

    统计初始化

--- application/common.php
+++ application/common.php
@@ -10,3 +10,38 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+//返回错误信息数组
+//1.返回的数据
+//2.返回码(默认200成功)
+//3.返回的正确(错误)信息
+function returnInfo($data = '', $retcode = 200, $msg = ''){
+    $tmp_arr             = array();
+    $tmp_arr['data']     = $data;
+    $tmp_arr['retcode'] = $retcode;
+    $tmp_arr['msg']     = $msg;
+    return $tmp_arr;
+}
+// XML    转成    数组
+//1.传入xml对象
+function xmlToArray($simpleXmlElement){
+    $simpleXmlElement=(array)$simpleXmlElement;
+    foreach($simpleXmlElement as $k=>$v){
+        if($v instanceof SimpleXMLElement ||is_array($v)){
+            $simpleXmlElement[$k]=xmlToArray($v);
+        }
+    }
+    return $simpleXmlElement;
+}
+//返回json格式
+//1.返回的数据
+//2.返回码(默认200成功)
+//3.返回的正确(错误)信息
+function returnJson($data = '', $retcode = 200, $msg = ''){
+    header('Content-Type: application/json');
+    $tmp_arr             = array();
+    $tmp_arr['data']    = $data;
+    $tmp_arr['retcode'] = $retcode;
+    $tmp_arr['msg']     = $msg;
+    echo json_encode($tmp_arr);
+    die;
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,8 +3,16 @@ namespace app\index\controller;
 
 class Index
 {
-    public function index()
-    {
-        return view('index');
+    public function index(){
+        $data = db('product')->limit(50)->order('soldNum')->select();
+
+
+        return view('index', ['data' => $data]);
+    }
+
+    public function ajaxBySoldNum(){
+        $data = db('product')->field('title,soldNum')->limit(50)->order('soldNum desc')->select();
+
+        return returnJson($data, 200, '成功');
     }
 }
