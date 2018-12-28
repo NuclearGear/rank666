8d91b909644b1520c63a4b6d4977c71da73b6946
levislin <levislin2016@gmail.com>
2018-12-28 13:45:47 +0800

    修改了Y轴显示为货号  上下销量顺序颠倒

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,13 +6,27 @@ class Index
     public function index(){
         $data = db('product')->limit(50)->order('soldNum')->select();
 
-
-        return view('index', ['data' => $data]);
+        return view('sold', ['data' => $data]);
     }
 
+    // 获取销量排名前50
     public function ajaxBySoldNum(){
-        $data = db('product')->field('title,soldNum')->limit(50)->order('soldNum desc')->select();
+        $data = db('product')->field('articleNumber,soldNum')->limit(50)->order('soldNum desc')->select();
 
         return returnJson($data, 200, '成功');
     }
+
+    // 5天内的所有信息
+    public function day5(){
+        // 获取昨日时间戳
+        $time_arr = Time::yesterdayNum(1);
+
+        $data = db('product')->limit(50)->order('soldNum')->select();
+        $ids = db('products_sold')->limit(50)->order('soldNum')->select();
+
+
+        return view('day5', ['data' => $data]);
+    }
+
+
 }
---
+++ application/model/productModel.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/27
+ * Time: 16:54
+ */
