9382f79cd8a41ae72a417a41d3d8e170ddfa4d13
levislin <levislin2016@gmail.com>
2018-12-30 10:37:14 +0800

    增加了单品统计，

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -28,5 +28,53 @@ class Index
         return view('day5', ['data' => $data]);
     }
 
+    // 单品统计
+    public function one(){
+        return view('one');
+
+    }
+
+    // 单品统计
+    public function AjaxProductOne(){
+        if (!input('?get.articleNumber')){
+            return returnJson('', 201, '请传商品Id！');
+        }
+        if (!input('?get.day')){
+            return returnJson('', 201, '请传天数！');
+        }
+        $data = [];
+
+        $product = db('product')->where(['articleNumber' => input('get.articleNumber')])->find();
+
+        $data['name'] = $product['title'];
+        $data['sizeName'] = [];
+        for($i=1;$i<=input('get.day');$i++){
+            $time = Time::yesterdayNum($i);
+
+            $ret_data = db('product_size')->where('spiderTime', 'between', $time)
+                                                ->where('productId', '=', $product['productId'])
+                                                ->select();
+            if ($ret_data){
+                $data['time'][] = date('Y-m-d', $time[0]);
+                foreach ($ret_data as $k => $v){
+                    $data['sizeName'][] = $v['size'];
+                    if ($v['size'] && $v['price']){
+                        $data[$v['size']][] = $v['price'] / 100;
+                    }
+                }
+            }
+        }
+        if ($data['sizeName']){
+            $data['sizeName'] = array_unique($data['sizeName']);
+        }
+
+
+
+
+
+
+        return returnJson($data, 200, '成功');
+    }
+
 
 }
