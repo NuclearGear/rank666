d6abac7ca9c7c3a04f4960695f2fc74f4f851004
levislin <levislin2016@gmail.com>
2019-01-24 18:16:50 +0800

    test

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,15 +18,20 @@ class Index
 
     // 获取差价数据
     public function diff(){
-        $data['where']['title'] = input('get.title');
-        $data['where']['sizeStart'] = input('get.sizeStart');
-        $data['where']['sizeEnd'] = input('get.sizeEnd');
-        $data['where']['diffPrice'] = input('get.diffPrice');
-        $data['where']['soldNum'] = input('get.soldNum');
+        $data['where']['title']         = input('get.title');
+        $data['where']['articleNumber'] = input('get.articleNumber');
+        $data['where']['sizeStart']     = input('get.sizeStart');
+        $data['where']['sizeEnd']       = input('get.sizeEnd');
+        $data['where']['diffPrice']     = input('get.diffPrice');
+        $data['where']['soldNum']       = input('get.soldNum');
+        $data['where']['ceil']          = input('get.ceil');
 
         $query = db('diff');
         if ($data['where']['title']){
             $query->where('title', 'like', '%' . input('get.title') . '%');
+        }
+        if ($data['where']['articleNumber']){
+            $query->where('articleNumber', 'like', '%' . input('get.articleNumber') . '%');
         }
         if ($data['where']['sizeStart']){
             $query->where('size', '>=', input('sizeStart'));
@@ -40,6 +45,10 @@ class Index
         if ($data['where']['soldNum']){
             $query->order('soldNum ' . input('get.soldNum'));
         }
+        if ($data['where']['ceil']){
+            $query->order('ceil ' . input('get.ceil'));
+        }
+
 
         $data['diff'] = $query->paginate(20);
 
