7c0636e5c10bf288e229a34ec11b73cac0ca1c9a
levislin <levislin2016@gmail.com>
2019-03-14 16:59:05 +0800

    完善了侧边栏  增加了 加入微信群

---
+++ application/index/controller/Shoes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\index\controller;
+
+
+use app\index\model\UserModel;
+use think\Controller;
+
+class Shoes extends Controller
+{
+    // 单品查询
+    public function index()
+    {
+        return view();
+    }
+
+}
