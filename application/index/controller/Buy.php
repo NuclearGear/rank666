8c356fe370c4eefb0fd9f4c7c25a5786c0208649
cccty <a18658377221@gmail.com>
2019-03-18 17:55:11 +0800

    Revert "test"
    This reverts commit a91d88f93ffccc9efefcc7cbb8a58a70f63b3248.

--- application/index/controller/Buy.php
+++ application/index/controller/Buy.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace app\index\controller;
-// use think\Request;
+
+
 use app\index\model\UserModel;
 use think\Controller;
 
@@ -13,11 +14,6 @@ class Buy extends Controller
     }
 
     public function ajax_add(){
-    	var_dump(input('post.'));exit;
-     	if (!input('post.name')){
-            return returnJson('', 201, '请填写 货号、尺码、成本、购买日期！');
-        }
-    	// var_dump(input('post.'));exit;
         return returnJson('', 200, '添加成功，请刷新页面后查看');
     }
 
