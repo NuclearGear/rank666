3b91dcdf922808b32818f26bef2b90d7bfa783d5
cccty <a18658377221@gmail.com>
2019-03-19 14:54:46 +0800

    优化利率

--- application/index/controller/Center.php
+++ application/index/controller/Center.php
@@ -32,14 +32,15 @@ class Center extends Controller
         // 已经出售
         $data['sold'] = BuyModel::where($where)->where('sold_price', 'NEQ', '')->count();
         // 本月利率
-        $data['interest_rate'] = $data['profit'] / $data['buy_cost'];
+        // $data['interest_rate'] = $data['profit'] / $data['buy_cost'];
+        $data['interest_rate'] = round($data['profit'] / $data['buy_cost'],3);
         // 上月利率
         if ($data['last_buy_cost'] == '0') {
         	$data['last_sold'] = '1';
         }else{
         	$data['last_sold'] = $data['last_buy_cost'];
         }
-        $data['last_interest_rate'] = $data['last_profit'] / $data['last_sold'];
+         $data['last_interest_rate'] = round( $data['last_profit'] / $data['last_sold'],2);
         return view('index', ['data' => $data]);
     }
 
