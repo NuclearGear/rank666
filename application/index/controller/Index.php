e4897eaeb0cbabc5b2e23f162e26b734bc8563f8
levislin <levislin2016@gmail.com>
2019-05-08 09:23:49 +0800

    update

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -403,6 +403,34 @@ class Index extends Controller
         return view('captcha');
     }
 
+    public function ajaxCaptcha(){
+        $data['secret'] = input('get.secret');
+        $data['response'] = input('get.response');
+        $secret = input('get.secret');
+
+        $ret = $this->request_post('https://www.recaptcha.net/recaptcha/api/siteverify', $data);
+        return returnJson($ret, 200, '成功');
+    }
+
+    function request_post($url = '', $param = '') {
+        if (empty($url) || empty($param)) {
+            return false;
+        }
+
+        $postUrl = $url;
+        $curlPost = $param;
+        $ch = curl_init();//初始化curl
+        curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
+        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        $data = curl_exec($ch);//运行curl
+        curl_close($ch);
+
+        return $data;
+    }
+
 
 
 }
