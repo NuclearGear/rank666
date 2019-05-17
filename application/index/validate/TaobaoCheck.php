7c19067b43b96cad8ff66e05ba3db33dc2535391
levislin <levislin2016@gmail.com>
2019-05-17 16:50:30 +0800

    udpate

--- application/index/validate/TaobaoCheck.php
+++ application/index/validate/TaobaoCheck.php
@@ -13,14 +13,13 @@ class TaobaoCheck extends Validate
 {
     protected $rule = [
         'article_number' => 'require',
-        'taobao_id'      => 'require|unique:TaobaoModel',
+        'taobao_id'      => 'require',
         'price'          => 'require',
     ];
 
     protected $message  =   [
         'article_number.require' => '请选择 对应鞋款',
         'taobao_id.require'      => '请填写 淘宝ID',
-        'taobao_id.unique'       => '淘宝ID已存在',
         'price.require'          => '请填写 溢价价格',
     ];
 
